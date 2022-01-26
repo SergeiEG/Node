8aace499afb7f6f5b8ec63f9a70821e8bb24f628
Sergei <1og@mail.ru>
2022-01-26 19:47:20 +0600

    working

--- index.js
+++ index.js
@@ -1,50 +1,33 @@
-const colors = require("colors/safe");
+const fs = require('fs');
+const { Transform } = require('stream');
+const ACCESS_LOG = './access.log';
+const ip = [
+    '89.123.1.41',
+    '34.48.240.111'
+]
 
-const args = process.argv.slice(2);
+let w = '127.0.0.1 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0" 89.123.1.41 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0" 127.0.0.1 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0"'
 
-const regexp = /^(\d+)\-(\d+)\-(\d+)\-(\d+)$/
+console.log(w.match(/89.123.1.41[^]*/g))
 
-class Timer {
-    constructor(str) {
-        this.futureDate = str
-    }
-    getDateFromArgs(str) {
-        let result = str.replace(regexp, (match, hour, day, month, year) => `${year}-${month}-${day}T${hour}`)
-        result = result + ':00:00.000'
-        let date = Date.parse(result)
-        return date
-    }
-    calcTime() {
-        let start = Date.now()
-        let end = this.getDateFromArgs(this.futureDate)
-        return end - start
-    }
-    showTimer() {
-        const timeId = setInterval(() => {
-            let ms = this.calcTime()
-            if (ms > 0) {
-                let days = Math.floor(ms / (1000 * 60 * 60 * 24));
-                let hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                let mins = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
-                let secs = Math.floor((ms % (1000 * 60)) / 1000);
+const readStream = fs.createReadStream(ACCESS_LOG, 'utf-8')
+ip.forEach(element => {
+    const writeSteam = fs.createWriteStream(`${element}_requests.log`, {
+        encoding: 'utf-8',
+        flags: 'a',
+    })
 
-                console.log(colors.yellow(`Осталось ${days} дней ${hours} часов ${mins}  минут ${secs}  секунд`))
-            } else {
-                console.log(colors.green('Завершение работы'))
-                clearInterval(timeId)
-            }
-        }, 1000);
-    }
-}
+    let regex = new RegExp(`^${element}[^]*\n`, 'g');
+    console.log(element)
+    console.log(regex)
 
-
-for (let i = 0; i < args.length; i++) {
-    if (regexp.test(args[i])) {
-        setTimeout(() => {
-            let timer = new Timer(args[i])
-            timer.showTimer()
-        })
-    } else {
-        console.log(colors.red(`Исправьте ${i+1} аргумент в формате час-день-месяц-год. Пример 10-01-01-2022`))
-    }
-}
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const transformedData = chunk.toString().match(regex);
+            console.log(transformedData);
+            this.push(transformedData);
+            callback();
+        }
+    })
+    readStream.pipe(transformStream).pipe(process.stdout);
+});
